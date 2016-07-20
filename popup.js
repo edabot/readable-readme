62a90793849893eb2d9aaa130db119e1f1b37904
edabot <ed.lewis@gmail.com>
2016-07-20 11:03:54 -0700

    updates for chrome store

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var blackcss = 'document.body.style.backgroundColor="black", document.body.style.color="black"'
 var redcss = 'document.body.style.backgroundColor="red", document.body.style.color="red"'
-var testcss = 'article.style.color="red"'
+// var testcss = 'article.style.color="red"'
 
 document.addEventListener('DOMContentLoaded', function() {
   var checkNormalButton = document.getElementById('normal-font');
