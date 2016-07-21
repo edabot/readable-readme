af6a62bc448cc544d6f65aaf411dbd6d7ecf437a
edabot <ed.lewis@gmail.com>
2016-07-21 12:10:03 -0700

    highlight fading added

--- checklist.js
+++ checklist.js
@@ -1,6 +1,7 @@
 var markdown = document.getElementsByClassName('markdown-body');
 var paragraphs = markdown[0].getElementsByTagName('p');
 var listItems = markdown[0].getElementsByTagName('li');
+var codeBlocks = markdown[0].getElementsByClassName('highlight');
 
 for (var i = 0; i < paragraphs.length; i++) {
   paragraphs[i].addEventListener("click", function() {
@@ -17,4 +18,8 @@ for (var i = 0; i < listItems.length; i++) {
   });
 }
 
-console.log('checklist.js loaded');
+for (var i = 0; i < codeBlocks.length; i++) {
+  codeBlocks[i].addEventListener("click", function() {
+    this.classList.toggle("faded");
+  });
+}
