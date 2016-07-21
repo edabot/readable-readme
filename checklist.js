08d102e367b603494d065db4ac4fb655a9d32a91
edabot <ed.lewis@gmail.com>
2016-07-21 11:22:47 -0700

    lis get fading and serif

--- checklist.js
+++ checklist.js
@@ -1,7 +1,18 @@
 var markdown = document.getElementsByClassName('markdown-body');
 var paragraphs = markdown[0].getElementsByTagName('p');
+var listItems = markdown[0].getElementsByTagName('li');
+
 for (var i = 0; i < paragraphs.length; i++) {
   paragraphs[i].addEventListener("click", function() {
+    this.classList.toggle("faded");
+  });
+  paragraphs[i].addEventListener("dragend", function() {
+    this.classList.toggle("faded");
+    console.log("dragged");
+  });
+}
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener("click", function() {
     this.classList.toggle("faded");
   });
 }
